6c5a6183f4f0c093d64b9958685505399856666e
Lubimov Igor <infunt@gmail.com>
2014-11-17 18:13:04 +0200

    fixed response

--- djucsvlog/fields/response.py
+++ djucsvlog/fields/response.py
@@ -2,17 +2,24 @@
 from djucsvlog import settings
 from djucsvlog.fields.tools import readable_dict
 
-def status(request,response):
+
+def status(request, response):
     return response.status_code
-def ctype(request,response):
-    if response.status_code == 304:
+
+
+def ctype(request, response):
+    if 'content-type' not in response._headers:
         return ''
     return response._headers['content-type'][1]
 
-def content(request,response):
-    if response.status_code == 304:
-        return ''
-    return response._headers['content-type'][1].lower() in settings.RESPONSE_CONTENT_LOG_TYPES and response.content or ''
 
-def headers(request,response):
+def content(request, response):
+    if 'content-type' not in response._headers:
+        return ''
+    if response._headers['content-type'][1].lower() in settings.RESPONSE_CONTENT_LOG_TYPES:
+        return response.content
+    return ''
+
+
+def headers(request, response):
     return readable_dict(response._headers)
